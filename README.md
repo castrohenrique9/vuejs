46db4c0e48ad6d199a98c88bdeabc97efceea650
Castro Henrique <castrohenrique9@gmail.com>
2020-03-13 10:22:31 -0300

    Organização de código

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # VueJS com Bootstrap4 consumindo end-point
 
+Este App utiliza Axios para consultar um end-point e mostrar Cards com o resultado.
+
 ## Entre na pasta do projeto e inicialize o container docker
 ```
 docker-compose up
 ```
 
-## A porta exposta é a 8085
+## Abra o App com a porta 8085
 http://localhost:8085
+
+Caso a porta 8085 já esteja sendo utilizada por outro App altere-a no arquivo docker-compose.yml
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <p>Este App consome um end-point com Axios e mostra a lista de Cards com o resultado</p>
   </div>
 </template>
 
@@ -15,18 +16,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+h1 {
+  margin: 20px;
 }
 </style>
